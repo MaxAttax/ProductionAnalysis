b6664584cd9f6be94b5bf89f909afefe4528221c
MaxAttax <maxattaxhoffmann@gmail.com>
2013-01-10 15:23:57 +0100

    update

--- src/org/maxhoffmann/dev/TestAnalysis.java
+++ src/org/maxhoffmann/dev/TestAnalysis.java
@@ -2,7 +2,7 @@ package org.maxhoffmann.dev;
 
 public class TestAnalysis {
 	
-	public static void main(String[] prg) {
+	public static void main(String[] args) {
 		System.out.println("Dies ist ein Test!");
 	}
 
