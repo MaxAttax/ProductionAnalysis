38c383f2acd48ea2c7c114605b3ae1d0fcca2dbc
MaxAttax <maxattaxhoffmann@gmail.com>
2013-01-10 17:17:39 +0100

    update

---
+++ src/org/maxhoffmann/dev/Resource.java
@@ -0,0 +1,16 @@
+package org.maxhoffmann.dev;
+
+import java.util.*;
+
+public class Resource
+{
+	private int id;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+}
--- src/org/maxhoffmann/dev/TestAnalysis.java
+++ src/org/maxhoffmann/dev/TestAnalysis.java
@@ -1,9 +1,21 @@
 package org.maxhoffmann.dev;
 
+import java.util.*;
+import org.hi
+
 public class TestAnalysis {
 	
+	private SessionFactory sessionFactory;
+	
 	public static void main(String[] args) {
-		System.out.println("Dies ist ein Test!");
+		
+		System.out.println("---------------");
+		System.out.println("Initializing Hibernate!");
+		sessionFactory = new Configuration().configure().buildSessionFactory();
+		
+		
+		
+		System.out.println("Dies ist ein Test!\nZweite Zeile");
 	}
 
 }
